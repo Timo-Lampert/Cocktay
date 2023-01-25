ae94cf8beeb655c7c20eb3bf867589abc75ca274
Timo <timolampert@web.de>
2023-01-25 17:28:23 +0100

    edit

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,9 +20,10 @@
 
     if (window.pageYOffset > sticky) {
       header_navbar.classList.add("sticky");
-      
+      logo.src = "../assets/img/logo/logo.svg";
+    } else {
       header_navbar.classList.remove("sticky");
-      
+      logo.src = "../assets/img/logo/logo.svg";
     }
 
     // show or hide the back-top-top button
